ee0bfadd483c41a8d70fbc2c2874d16c878235c2
Matt Hadden <forceh2010@gmail.com>
2026-03-14 21:42:17 -0400

    refactor(collision): give back more info on satcollision with new struct

--- src/core/collision.hh
+++ src/core/collision.hh
@@ -3,13 +3,20 @@
 
 #include "object/gameobject.hh"
 #include "collision_types.hh"
+#include "psyqo/fixed-point.hh"
+
+struct CollisionTest {
+    psyqo::Vec3 mtv;
+    psyqo::Vec3 normal;
+    psyqo::FixedPoint<> penetration;
+};
 
 class Collision
 {
 public:
     static void GenerateAABBForMesh(const GameObject *object, AABBCollision *collisionBoxOut);
     static bool IsAABBCollision(const AABBCollision &collisionA, const AABBCollision &collisionB);
-    static bool IsSATCollision(const OBB &collisionA, const OBB &collisionB);
+    static bool IsSATCollision(const OBB &collisionA, const OBB &collisionB, CollisionTest *resultOut);
 };
 
 #endif
