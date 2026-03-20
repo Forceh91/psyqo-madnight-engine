f9f0e31367f1432f5b66dc05296bdfb638bffcec
Matt Hadden <forceh2010@gmail.com>
2026-03-19 20:24:24 -0400

    refactor(loading): into a switch/case

--- src/scenes/loading.cpp
+++ src/scenes/loading.cpp
@@ -1,12 +1,11 @@
 #include "loading.hh"
 #include "../animation/animation_manager.hh"
+#include "../core/debug//perf_monitor.hh"
+#include "../mesh/colbin_manager.hh"
 #include "../mesh/mesh_manager.hh"
 #include "../render/renderer.hh"
 #include "../sound/sound_manager.hh"
 #include "../textures/texture_manager.hh"
-#include "../mesh/colbin_manager.hh"
-#include "../core/debug//perf_monitor.hh"
-
 
 #include "psyqo/fixed-point.hh"
 
@@ -14,53 +13,67 @@ void LoadingScene::start(StartReason reason) { Renderer::Instance().StartScene()
 
 void LoadingScene::frame() {
   uint32_t deltaTime = Renderer::Instance().Process();
-  if (deltaTime == 0)
-    return;
+	if (deltaTime == 0)
+		return;
 
-  auto loaded = psyqo::FixedPoint<>(int32_t(m_loadFilesLoadedCount), int32_t(0));
-  auto total = psyqo::FixedPoint<>(int32_t(m_loadFilesCount), int32_t(0));
-  auto percent = loaded > 0 ? loaded / total * 100 : 0;
+	auto loaded = psyqo::FixedPoint<>(int32_t(m_loadFilesLoadedCount), int32_t(0));
+	auto total = psyqo::FixedPoint<>(int32_t(m_loadFilesCount), int32_t(0));
+	auto percent = loaded > 0 ? loaded / total * 100 : 0;
 
-  Renderer::Instance().RenderLoadingScreen(percent.integer());
-  PerfMonitor::Render(deltaTime);
+	Renderer::Instance().RenderLoadingScreen(percent.integer());
+	PerfMonitor::Render(deltaTime);
 }
 
 psyqo::Coroutine<> LoadingScene::LoadFiles(eastl::vector<LoadQueue> &&files, bool dumpExisting) {
-  // most likely we want to do this, but this will dump everything we know
-  // about meshes and textures, ready for a fresh scene
-  if (dumpExisting) {
-    MeshManager::Dump();
-    TextureManager::Dump();
-    ColbinManager::Dump();
-  }
+	// most likely we want to do this, but this will dump everything we know
+	// about meshes and textures, ready for a fresh scene
+	if (dumpExisting) {
+		MeshManager::Dump();
+		TextureManager::Dump();
+		ColbinManager::Dump();
+	}
 
-  m_queue = eastl::move(files);
-  m_loadFilesCount = m_queue.size();
-  m_loadFilesLoadedCount = 0;
+	m_queue = eastl::move(files);
+	m_loadFilesCount = m_queue.size();
+	m_loadFilesLoadedCount = 0;
 
-  // load it backwards so we can erase as we go
-  for (int i = m_queue.size() - 1; i >= 0; i--) {
-    auto const &file = m_queue.at(i);
+	// load it backwards so we can erase as we go
+	for (int i = m_queue.size() - 1; i >= 0; i--) {
+		auto const &file = m_queue.at(i);
 
-    MeshBin *mesh = nullptr;
-    TimFile *tim = nullptr;
-    ModSoundFile *modSound = nullptr;
-    ColBin *colbin = nullptr;
-    if (file.type == LoadFileType::OBJECT)
-      co_await MeshManager::LoadMeshFromCDROM(file.name.c_str(), &mesh);
-    if (file.type == LoadFileType::TEXTURE)
-      co_await TextureManager::LoadTIMFromCDRom(file.name.c_str(), file.x, file.y, file.clutX, file.clutY, &tim);
-    if (file.type == LoadFileType::MOD_FILE)
-      co_await SoundManager::LoadMODSoundFromCDRom(file.name.c_str(), &modSound);
-    if (file.type == LoadFileType::ANIMATION)
-      co_await AnimationManager::LoadAnimationFromCDRom(file.name.c_str());
-    if (file.type == LoadFileType::COLBIN)
-      co_await ColbinManager::LoadColbin(file.name, &colbin);
+		switch (file.type) {
+      case LoadFileType::OBJECT: {
+        MeshBin *mesh = nullptr;
+        co_await MeshManager::LoadMeshFromCDROM(file.name.c_str(), &mesh);
+        break;
+      }
 
-    m_queue.erase(m_queue.begin() + i);
+      case LoadFileType::TEXTURE: {
+        TimFile *tim = nullptr;
+        co_await TextureManager::LoadTIMFromCDRom(file.name.c_str(), file.x, file.y, file.clutX, file.clutY, &tim);
+        break;
+      }
 
-    m_loadFilesLoadedCount++;
-  }
+      case LoadFileType::MOD_FILE: {
+        ModSoundFile *modSound = nullptr;
+        co_await SoundManager::LoadMODSoundFromCDRom(file.name.c_str(), &modSound);
+        break;
+      }
 
-  m_queue.clear();
+      case LoadFileType::ANIMATION:
+        co_await AnimationManager::LoadAnimationFromCDRom(file.name.c_str());
+        break;
+
+      case LoadFileType::COLBIN: {
+        ColBin *colbin = nullptr;
+        co_await ColbinManager::LoadColbin(file.name, &colbin);
+        break;
+      }
+		}
+
+		m_queue.erase(m_queue.begin() + i);
+		m_loadFilesLoadedCount++;
+	}
+
+	m_queue.clear();
 }
