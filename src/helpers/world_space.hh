1ff9bbea964279fe5e3a5380560c080917284a0e
Matt Hadden <Forceh91@users.noreply.github.com>
2026-08-08 21:07:13 -0400

    fix(rand): randomizer getting stuck on the same result (#34)

--- src/helpers/world_space.hh
+++ src/helpers/world_space.hh
@@ -8,6 +8,7 @@ using namespace psyqo::fixed_point_literals;
 // right now exporting from blender, 0.5 is 1m, 1.0 is 2m, etc.
 // 1m, 0.5 blender units = 0.5_ws
 // 2m, 1 blender units = 1.0_ws
+// basically if blender says X is 50m, then you want 25.0_ws, if it says 1m, you want 0.5_ws
 consteval psyqo::FixedPoint<> operator""_ws(long double blenderUnits) {
     psyqo::FixedPoint<> output;
     output.value = blenderUnits * 128;
