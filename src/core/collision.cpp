ade8f789dd08d8518759a1c19f221db284730feb
Matt Hadden <Forceh91@users.noreply.github.com>
2026-09-12 18:37:35 -0400

    refactor(collision): swap out `psyqo::SoftMath` for `psyqo::GteMath`

--- src/core/collision.cpp
+++ src/core/collision.cpp
@@ -2,156 +2,158 @@
 #include "../math/gte-math.hh"
 #include "../math/vector.hh"
 #include "../mesh/mesh_manager.hh"
-#include "psyqo/fixed-point.hh"
-#include "psyqo/soft-math.hh"
-#include "psyqo/vector.hh"
+#include <psyqo/fixed-point.hh>
+#include <psyqo/gte-math.hh>
+#include <psyqo/vector.hh>
 
 // TODO: cache these AABBs somewhere?
 // TODO: this obviously gets weird with rotated objects so might need to update later
 [[deprecated("This should be done offline. DO NOT USE")]]
-void Collision::GenerateAABBForMesh(const GameObject *object, AABBCollision *collisionBoxOut) {
-  const MeshBin *mesh = object->mesh();
+void Collision::GenerateAABBForMesh(const GameObject* object, AABBCollision* collisionBoxOut) {
+	const MeshBin* mesh = object->mesh();
 
-  // make sure it has faces
-  if (mesh->facesCount == 0)
-    return;
+	// make sure it has faces
+	if (mesh->facesCount == 0)
+		return;
 
-  // vec for min/max points
-  collisionBoxOut->min.x = UINT16_MAX;
-  collisionBoxOut->min.y = UINT16_MAX;
-  collisionBoxOut->min.z = UINT16_MAX;
-  collisionBoxOut->max.x = -UINT16_MAX;
-  collisionBoxOut->max.y = -UINT16_MAX;
-  collisionBoxOut->max.z = -UINT16_MAX;
+	// vec for min/max points
+	collisionBoxOut->min.x = UINT16_MAX;
+	collisionBoxOut->min.y = UINT16_MAX;
+	collisionBoxOut->min.z = UINT16_MAX;
+	collisionBoxOut->max.x = -UINT16_MAX;
+	collisionBoxOut->max.y = -UINT16_MAX;
+	collisionBoxOut->max.z = -UINT16_MAX;
 
-  psyqo::Vec3 transformedPos = {0};
-  psyqo::SoftMath::matrixVecMul3(object->rotationMatrix(), object->pos(), &transformedPos);
+	psyqo::Vec3 transformedPos = {0};
+	psyqo::GteMath::matrixVecMul3(object->rotationMatrix(), object->pos(), &transformedPos);
 
-  // for each vertex on the mesh
-  for (uint16_t vert = 0; vert < mesh->vertexCount; vert++) {
-    // apply object rotation to the vert positions
-    auto vertPos = mesh->vertices[vert];
+	// for each vertex on the mesh
+	for (uint16_t vert = 0; vert < mesh->vertexCount; vert++) {
+		// apply object rotation to the vert positions
+		auto vertPos = mesh->vertices[vert];
 
-    // mins
-    if (vertPos.x < collisionBoxOut->min.x)
-      collisionBoxOut->min.x = vertPos.x;
+		// mins
+		if (vertPos.x < collisionBoxOut->min.x)
+			collisionBoxOut->min.x = vertPos.x;
 
-    if (vertPos.y < collisionBoxOut->min.y)
-      collisionBoxOut->min.y = vertPos.y;
+		if (vertPos.y < collisionBoxOut->min.y)
+			collisionBoxOut->min.y = vertPos.y;
 
-    if (vertPos.z < collisionBoxOut->min.z)
-      collisionBoxOut->min.z = vertPos.z;
+		if (vertPos.z < collisionBoxOut->min.z)
+			collisionBoxOut->min.z = vertPos.z;
 
-    // maxes
-    if (vertPos.x > collisionBoxOut->max.x)
-      collisionBoxOut->max.x = vertPos.x;
+		// maxes
+		if (vertPos.x > collisionBoxOut->max.x)
+			collisionBoxOut->max.x = vertPos.x;
 
-    if (vertPos.y > collisionBoxOut->max.y)
-      collisionBoxOut->max.y = vertPos.y;
+		if (vertPos.y > collisionBoxOut->max.y)
+			collisionBoxOut->max.y = vertPos.y;
 
-    if (vertPos.z > collisionBoxOut->max.z)
-      collisionBoxOut->max.z = vertPos.z;
-  }
+		if (vertPos.z > collisionBoxOut->max.z)
+			collisionBoxOut->max.z = vertPos.z;
+	}
 }
 
 // this is super simple and doesn't care about rotations
-bool Collision::IsAABBCollision(const AABBCollision &collisionA, const AABBCollision &collisionB) {
-  // AABB box: check the min/max X
-  if (collisionA.max.x < collisionB.min.x || collisionA.min.x > collisionB.max.x)
-    return false;
+bool Collision::IsAABBCollision(const AABBCollision& collisionA, const AABBCollision& collisionB) {
+	// AABB box: check the min/max X
+	if (collisionA.max.x < collisionB.min.x || collisionA.min.x > collisionB.max.x)
+		return false;
 
-  // AABB box: check the min/max Y
-  if (collisionA.max.y < collisionB.min.y || collisionA.min.y > collisionB.max.y)
-    return false;
+	// AABB box: check the min/max Y
+	if (collisionA.max.y < collisionB.min.y || collisionA.min.y > collisionB.max.y)
+		return false;
 
-  // AABB box: check the min/max Z
-  if (collisionA.max.z < collisionB.min.z || collisionA.min.z > collisionB.max.z)
-    return false;
+	// AABB box: check the min/max Z
+	if (collisionA.max.z < collisionB.min.z || collisionA.min.z > collisionB.max.z)
+		return false;
 
-  // ok so we know we're colliding
-  return true;
+	// ok so we know we're colliding
+	return true;
 }
 
 // this is more complicated and does take into account object rotation properly
-bool Collision::IsSATCollision(const OBB &collisionA, const OBB &collisionB, CollisionTest *resultOut) {
-  auto minACentre = 0.0_fp, minBCentre = 0.0_fp;
+bool Collision::IsSATCollision(const OBB& collisionA, const OBB& collisionB, CollisionTest* resultOut) {
+	auto minACentre = 0.0_fp, minBCentre = 0.0_fp;
 
-  resultOut->mtv = psyqo::Vec3::ZERO();
-  resultOut->normal = psyqo::Vec3::ZERO();
-  resultOut->penetration = 1000.0_fp;
+	resultOut->mtv = psyqo::Vec3::ZERO();
+	resultOut->normal = psyqo::Vec3::ZERO();
+	resultOut->penetration = 1000.0_fp;
 
-  // get all of the axes that we want to check
-  // this is the 3 from object a, 3 object b, and 9 from the cross product of all a+b combos
-  eastl::array<psyqo::Vec3, 15> axesToCheck = {
-      collisionA.axes[0],
-      collisionA.axes[1],
-      collisionA.axes[2],
-      collisionB.axes[0],
-      collisionB.axes[1],
-      collisionB.axes[2],
-      psyqo::SoftMath::crossProductVec3(collisionA.axes[0], collisionB.axes[0]),
-      psyqo::SoftMath::crossProductVec3(collisionA.axes[0], collisionB.axes[1]),
-      psyqo::SoftMath::crossProductVec3(collisionA.axes[0], collisionB.axes[2]),
-      psyqo::SoftMath::crossProductVec3(collisionA.axes[1], collisionB.axes[0]),
-      psyqo::SoftMath::crossProductVec3(collisionA.axes[1], collisionB.axes[1]),
-      psyqo::SoftMath::crossProductVec3(collisionA.axes[1], collisionB.axes[2]),
-      psyqo::SoftMath::crossProductVec3(collisionA.axes[2], collisionB.axes[0]),
-      psyqo::SoftMath::crossProductVec3(collisionA.axes[2], collisionB.axes[1]),
-      psyqo::SoftMath::crossProductVec3(collisionA.axes[2], collisionB.axes[2])};
+	psyqo::Vec3 collisionA0B0 = {0}, collisionA0B1 = {0}, collisionA0B2 = {0}, collisionA1B0 = {0}, collisionA1B1 = {0},
+				collisionA1B2 = {0}, collisionA2B0 = {0}, collisionA2B1 = {0}, collisionA2B2 = {0};
 
-  // we can skip this axes if we get a vec3 zero for any of these
-  auto i = -1;
-  for (auto &axis : axesToCheck) {
-    i++;
-    // skip this one if its zero
-    if (axis.x == 0 && axis.y == 0 && axis.z == 0)
-      continue;
+	psyqo::GteMath::crossProductVec3(collisionA.axes[0], collisionB.axes[0], &collisionA0B0);
+	psyqo::GteMath::crossProductVec3(collisionA.axes[0], collisionB.axes[1], &collisionA0B1);
+	psyqo::GteMath::crossProductVec3(collisionA.axes[0], collisionB.axes[2], &collisionA0B2);
+	psyqo::GteMath::crossProductVec3(collisionA.axes[1], collisionB.axes[0], &collisionA1B0);
+	psyqo::GteMath::crossProductVec3(collisionA.axes[1], collisionB.axes[1], &collisionA1B1);
+	psyqo::GteMath::crossProductVec3(collisionA.axes[1], collisionB.axes[2], &collisionA1B2);
+	psyqo::GteMath::crossProductVec3(collisionA.axes[2], collisionB.axes[0], &collisionA2B0);
+	psyqo::GteMath::crossProductVec3(collisionA.axes[2], collisionB.axes[1], &collisionA2B1);
+	psyqo::GteMath::crossProductVec3(collisionA.axes[2], collisionB.axes[2], &collisionA2B2);
 
-    // normalize it
-    auto normalizedAxis = axis;
-    psyqo::SoftMath::normalizeVec3(&normalizedAxis);
+	eastl::array<psyqo::Vec3, 15> axesToCheck = {
+		collisionA.axes[0], collisionA.axes[1], collisionA.axes[2], collisionB.axes[0], collisionB.axes[1],
+		collisionB.axes[2], collisionA0B0,		collisionA0B1,		collisionA0B2,		collisionA1B0,
+		collisionA1B1,		collisionA1B2,		collisionA2B0,		collisionA2B1,		collisionA2B2,
+	};
 
-    // centre dot products
-    auto aCentre = DotProduct(collisionA.center, normalizedAxis);
-    auto bCentre = DotProduct(collisionB.center, normalizedAxis);
+	// we can skip this axes if we get a vec3 zero for any of these
+	auto i = -1;
+	for (auto& axis : axesToCheck) {
+		i++;
+		// skip this one if its zero
+		if (axis.x == 0 && axis.y == 0 && axis.z == 0)
+			continue;
 
-    // use the GTE to project the axes for us
-    psyqo::Vec3 projectionA =
-        GTEMath::ProjectVectorOntoAxes({collisionA.axes[0], collisionA.axes[1], collisionA.axes[2]}, normalizedAxis);
-    psyqo::Vec3 projectionB =
-        GTEMath::ProjectVectorOntoAxes({collisionB.axes[0], collisionB.axes[1], collisionB.axes[2]}, normalizedAxis);
+		// normalize it
+		auto normalizedAxis = axis;
+		psyqo::GteMath::normalizeVec3(&normalizedAxis);
 
-    // now project the half extents
-    auto aRadius = (projectionA.x.abs() * collisionA.halfExtents.x) + (projectionA.y.abs() * collisionA.halfExtents.y) +
-                   (projectionA.z.abs() * collisionA.halfExtents.z);
-    auto bRadius = (projectionB.x.abs() * collisionB.halfExtents.x) + (projectionB.y.abs() * collisionB.halfExtents.y) +
-                   (projectionB.z.abs() * collisionB.halfExtents.z);
+		// centre dot products
+		auto aCentre = DotProduct(collisionA.center, normalizedAxis);
+		auto bCentre = DotProduct(collisionB.center, normalizedAxis);
 
-    // check for overlap
-    auto distance = (aCentre - bCentre).abs();
-    auto overlap = aRadius + bRadius;
-    auto penetration = overlap - distance;
+		// use the GTE to project the axes for us
+		psyqo::Vec3 projectionA = GTEMath::ProjectVectorOntoAxes(
+			{collisionA.axes[0], collisionA.axes[1], collisionA.axes[2]}, normalizedAxis);
+		psyqo::Vec3 projectionB = GTEMath::ProjectVectorOntoAxes(
+			{collisionB.axes[0], collisionB.axes[1], collisionB.axes[2]}, normalizedAxis);
 
-    // no overlap if distance is greater than radius
-    if (distance > overlap)
-      return false;
+		// now project the half extents
+		auto aRadius = (projectionA.x.abs() * collisionA.halfExtents.x) +
+					   (projectionA.y.abs() * collisionA.halfExtents.y) +
+					   (projectionA.z.abs() * collisionA.halfExtents.z);
+		auto bRadius = (projectionB.x.abs() * collisionB.halfExtents.x) +
+					   (projectionB.y.abs() * collisionB.halfExtents.y) +
+					   (projectionB.z.abs() * collisionB.halfExtents.z);
 
-    // skip axis on negative penetration
-    if (penetration <= 0)
-      continue;
+		// check for overlap
+		auto distance = (aCentre - bCentre).abs();
+		auto overlap = aRadius + bRadius;
+		auto penetration = overlap - distance;
 
-    // keep track of what the min penetration and axes we're pushed on is
-    if (penetration < resultOut->penetration) {
-      resultOut->penetration = penetration;
+		// no overlap if distance is greater than radius
+		if (distance > overlap)
+			return false;
 
-      resultOut->normal = normalizedAxis;
-      if (aCentre < bCentre)
-        resultOut->normal = -resultOut->normal;
+		// skip axis on negative penetration
+		if (penetration <= 0)
+			continue;
 
-      resultOut->mtv = resultOut->normal * resultOut->penetration;
-    }
-  }
-  
-  // no separated axis found, so we must be colliding
-  return true;
+		// keep track of what the min penetration and axes we're pushed on is
+		if (penetration < resultOut->penetration) {
+			resultOut->penetration = penetration;
+
+			resultOut->normal = normalizedAxis;
+			if (aCentre < bCentre)
+				resultOut->normal = -resultOut->normal;
+
+			resultOut->mtv = resultOut->normal * resultOut->penetration;
+		}
+	}
+
+	// no separated axis found, so we must be colliding
+	return true;
 }
--- src/core/collision.hh
+++ src/core/collision.hh
@@ -1,19 +1,18 @@
 #pragma once
 
-#include "object/gameobject.hh"
 #include "collision_types.hh"
-#include "psyqo/fixed-point.hh"
+#include "object/gameobject.hh"
+#include <psyqo/fixed-point.hh>
 
 struct CollisionTest {
-    psyqo::Vec3 mtv;
-    psyqo::Vec3 normal;
-    psyqo::FixedPoint<> penetration;
+	psyqo::Vec3 mtv;
+	psyqo::Vec3 normal;
+	psyqo::FixedPoint<> penetration;
 };
 
-class Collision
-{
-public:
-    static void GenerateAABBForMesh(const GameObject *object, AABBCollision *collisionBoxOut);
-    static bool IsAABBCollision(const AABBCollision &collisionA, const AABBCollision &collisionB);
-    static bool IsSATCollision(const OBB &collisionA, const OBB &collisionB, CollisionTest *resultOut);
+class Collision {
+  public:
+	static void GenerateAABBForMesh(const GameObject* object, AABBCollision* collisionBoxOut);
+	static bool IsAABBCollision(const AABBCollision& collisionA, const AABBCollision& collisionB);
+	static bool IsSATCollision(const OBB& collisionA, const OBB& collisionB, CollisionTest* resultOut);
 };
