feec27e9de762ca09dc2c75e545926c7cee7e088
Matt Hadden <Forceh91@users.noreply.github.com>
2026-09-12 17:32:08 -0400

    chore(texture): format

--- src/textures/texture_manager.hh
+++ src/textures/texture_manager.hh
@@ -1,10 +1,10 @@
 #pragma once
 
-#include <stdint.h>
-#include <EASTL/functional.h>
-#include <EASTL/string_view.h>
 #include "psyqo/coroutine.hh"
 #include "psyqo/primitives.hh"
+#include <EASTL/functional.h>
+#include <EASTL/string_view.h>
+#include <stdint.h>
 
 static constexpr uint16_t texturePageWidth = 64;
 static constexpr uint16_t texturePageHeight = 256;
@@ -14,39 +14,38 @@ static constexpr uint8_t MAX_TEXTURES = 32; // this will need tweaking later
 // a value VRAM can never hold. VRAM is 1024x512.
 static constexpr uint16_t TIM_POSITION_FROM_FILE = 0xFFFF;
 
-typedef struct _TIM_FILE
-{
-    uint64_t nameHash;
-    bool isLoaded;                                // is this slot actually holding a texture?
-    uint16_t x, y, width, height;                 // pos in vram + width/height
-    psyqo::Prim::TPageAttr::ColorMode colourMode; // bits per pixel (4, 8, 16)
+typedef struct _TIM_FILE {
+	uint64_t nameHash;
+	bool isLoaded;								  // is this slot actually holding a texture?
+	uint16_t x, y, width, height;				  // pos in vram + width/height
+	psyqo::Prim::TPageAttr::ColorMode colourMode; // bits per pixel (4, 8, 16)
 
-    bool hasClut;                   // does it need/have a clut?
-    uint16_t clutX, clutY;          // clut pos in vram
-    uint16_t clutWidth, clutHeight; // clut width and height (always 1)
+	bool hasClut;					// does it need/have a clut?
+	uint16_t clutX, clutY;			// clut pos in vram
+	uint16_t clutWidth, clutHeight; // clut width and height (always 1)
 } TimFile;
 
-class TextureManager final
-{
-    static psyqo::Vertex GetTPageIndex(uint16_t x, uint16_t y);
-    static eastl::array<TimFile, MAX_TEXTURES> m_textures;
+class TextureManager final {
+	static psyqo::Vertex GetTPageIndex(uint16_t x, uint16_t y);
+	static eastl::array<TimFile, MAX_TEXTURES> m_textures;
 
-    static int16_t GetFreeIndex(void);
-    static TimFile *IsTextureLoaded(const eastl::string_view& name);
-    static TimFile *IsTextureLoaded(uint64_t nameHash);
+	static int16_t GetFreeIndex(void);
+	static TimFile* IsTextureLoaded(const eastl::string_view& name);
+	static TimFile* IsTextureLoaded(uint64_t nameHash);
 
-public:
-    static psyqo::Coroutine<> LoadTIM(const eastl::string_view& textureName, uint16_t x, uint16_t y, uint16_t clutX, uint16_t clutY, TimFile **timOut);
-    static psyqo::PrimPieces::TPageAttr GetTPageAttr(const TimFile *tim);
-    static psyqo::PrimPieces::TPageAttr GetTPageAttr(const TimFile &tim);
-    static psyqo::Rect GetTPageUVForTim(const TimFile &tim);
-    static psyqo::Rect GetTPageUVForTim(const TimFile *tim);
+  public:
+	static psyqo::Coroutine<> LoadTIM(const eastl::string_view& textureName, uint16_t x, uint16_t y, uint16_t clutX,
+									  uint16_t clutY, TimFile** timOut);
+	static psyqo::PrimPieces::TPageAttr GetTPageAttr(const TimFile* tim);
+	static psyqo::PrimPieces::TPageAttr GetTPageAttr(const TimFile& tim);
+	static psyqo::Rect GetTPageUVForTim(const TimFile& tim);
+	static psyqo::Rect GetTPageUVForTim(const TimFile* tim);
 
-    static void GetTextureFromName(const eastl::string_view& textureName, TimFile **timOut);
+	static void GetTextureFromName(const eastl::string_view& textureName, TimFile** timOut);
 
-    // dump all textures in memory and start fresh
-    // this is used when switching to a loading screen for instance.
-    // this is a dangerous function as it wont check if anything is used
-    // this wont remove anything from vram
-    static void Dump(void);
+	// dump all textures in memory and start fresh
+	// this is used when switching to a loading screen for instance.
+	// this is a dangerous function as it wont check if anything is used
+	// this wont remove anything from vram
+	static void Dump(void);
 };
