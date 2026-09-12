546e23ba6798702332c0eab3fde50d0aeb52b0f2
Matt Hadden <Forceh91@users.noreply.github.com>
2026-09-12 17:19:21 -0400

    chore(gameobject): format with tabs

--- src/core/object/gameobject.hh
+++ src/core/object/gameobject.hh
@@ -64,39 +64,39 @@ class GameObject final {
 	GameObject() = default;
 
 	uint64_t nameHash() const { return m_nameHash; }
-	const eastl::fixed_string<char, MAX_GAMEOBJECT_NAME_LENGTH> &name() const { return m_name; }
-	const uint8_t &id() const { return m_id; };
-	const psyqo::Vec3 &pos() const { return m_pos; }
+	const eastl::fixed_string<char, MAX_GAMEOBJECT_NAME_LENGTH>& name() const { return m_name; }
+	const uint8_t& id() const { return m_id; };
+	const psyqo::Vec3& pos() const { return m_pos; }
 
-	const psyqo::Vec3 *posPtr() const { return &m_pos; }
-	psyqo::Vec3 *posPtr() { return &m_pos; }
+	const psyqo::Vec3* posPtr() const { return &m_pos; }
+	psyqo::Vec3* posPtr() { return &m_pos; }
 
-	const GameObjectRotation &rotation() const { return m_rotation; }
-	const psyqo::Matrix33 &rotationMatrix() const { return m_rotationMatrix; }
-	const MeshBin *mesh() const { return m_mesh; }
-	MeshBin *mesh() { return m_mesh; }
-	const TimFile *texture() const { return m_texture; }
-	const GameObjectTag &tag() { return m_tag; }
-	const GameObjectQuadType &quadType() { return m_quadType; }
-	const OBB &obb() { return m_obb; }
-	const OBB &obb() const { return m_obb; }
+	const GameObjectRotation& rotation() const { return m_rotation; }
+	const psyqo::Matrix33& rotationMatrix() const { return m_rotationMatrix; }
+	const MeshBin* mesh() const { return m_mesh; }
+	MeshBin* mesh() { return m_mesh; }
+	const TimFile* texture() const { return m_texture; }
+	const GameObjectTag& tag() { return m_tag; }
+	const GameObjectQuadType& quadType() { return m_quadType; }
+	const OBB& obb() { return m_obb; }
+	const OBB& obb() const { return m_obb; }
 
-	void SetPosition(const psyqo::Vec3 &pos);
+	void SetPosition(const psyqo::Vec3& pos);
 	void SetPosition(psyqo::FixedPoint<12> x, psyqo::FixedPoint<12> y, psyqo::FixedPoint<12> z);
-	void SetRotation(const GameObjectRotation &rotation);
+	void SetRotation(const GameObjectRotation& rotation);
 	void SetRotation(psyqo::Angle x, psyqo::Angle y, psyqo::Angle z);
 	void SetMesh(const char *meshName);
 	void SetTexture(const char *textureName);
 	// note: doesn't actually do anything yet. need to figure it out later when its important
 	void SetQuadType(const GameObjectQuadType quadType) { m_quadType = quadType; }
-	void SetAsTrigger(const psyqo::Vec3 &size);
-	bool HasRenderFlag(const RenderFlags &rf) { return m_renderFlags & (1 << rf); }
-	void SetRenderFlag(const RenderFlags &rf) { m_renderFlags |= (1 << rf); }
-	void ClearRenderFlag(const RenderFlags &rf) { m_renderFlags &= ~(1 << rf); }
+	void SetAsTrigger(const psyqo::Vec3& size);
+	bool HasRenderFlag(const RenderFlags& rf) { return m_renderFlags & (1 << rf); }
+	void SetRenderFlag(const RenderFlags& rf) { m_renderFlags |= (1 << rf); }
+	void ClearRenderFlag(const RenderFlags& rf) { m_renderFlags &= ~(1 << rf); }
 	void ClearRenderFlags(void) { m_renderFlags = RF_NONE; }
 
-	bool HasFlag(const uint32_t &rf) { return m_flags & (1 << rf); }
-	void SetFlag(const uint32_t &rf) { m_flags |= (1 << rf); }
-	void ClearFlag(const uint32_t &rf) { m_flags &= ~(1 << rf); }
+	bool HasFlag(const uint32_t& rf) { return m_flags & (1 << rf); }
+	void SetFlag(const uint32_t& rf) { m_flags |= (1 << rf); }
+	void ClearFlag(const uint32_t& rf) { m_flags &= ~(1 << rf); }
 	void ClearFlags(void) { m_flags = 0; }
 };
